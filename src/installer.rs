4bc7914de1877fa3d1f6265141603da6b6b5b273
Miguel A. Arilla <mangelarilla@users.noreply.github.com>
2022-10-17 17:22:06 +0200

    bypass windows smartscreen

--- src/installer.rs
+++ src/installer.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, io, process::Command, path::PathBuf};
+use std::{env, fs, io, path::PathBuf};
 use crate::config::{self, SystemDir};
 
 const EXE_PATH: &str = r".anyconnect-auto\anyconnect-auto.exe";
@@ -14,11 +14,7 @@ pub fn copy_executable() -> io::Result<PathBuf> {
 }
 
 pub fn link_to_windows_startup() -> io::Result<()> {
-    let shortcut = config::from_system_dir(r"Microsoft\Windows\Start Menu\Programs\Startup\anyconnect-auto.lnk", SystemDir::APPDATA);
+    let startup_cmd = config::from_system_dir(r"Microsoft\Windows\Start Menu\Programs\Startup\anyconnect-auto.cmd", SystemDir::APPDATA);
     let exe = config::from_system_dir(EXE_PATH, SystemDir::USERPROFILE);
-    Command::new("powershell")
-        .arg(format!("$s=(New-Object -COM WScript.Shell).CreateShortcut('{}');$s.TargetPath='{}';$s.Save()", shortcut.to_string_lossy(), exe.to_string_lossy()))
-        .spawn()?
-        .wait()?;
-    Ok(())
+    fs::write(startup_cmd, format!("{}\nexit", exe.to_string_lossy()))
 }
